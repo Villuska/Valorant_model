0536f43a7c3925ced642bdf5265729a8863b3804
ville roth <Villuska>
2024-11-27 23:25:33 +0200

    public setting test

--- tmp.py
+++ tmp.py
@@ -6,4 +6,5 @@ df = pd.read_csv("player_team_data.csv")
 """
 ekaks helpers.py kuntoon sit haetaan save df as csv ja se toinen
 scrapee tiimit uudestaan mut tällä kertaa ota myös linkit + aja nimet helpersin kautta aina 
+testataan
 """
